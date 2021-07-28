6d3eb4c37b2bfcac41f4e74c0b6c1459e34bd768
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-07-29 00:27:20 +0900

    fix apis

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { getPosts } from './actions/posts';
 import memories from './images/memories.jpeg';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -7,6 +9,11 @@ import useStyles from './styles';
 
 const App = () => {
 	const classes = useStyles();
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getPosts());
+	}, [dispatch])
 
 	return (
 		<div>
---
+++ client/src/actions/posts.js
@@ -0,0 +1,16 @@
+import * as api from '../api/index.js';
+
+// action creators
+export const getPosts = () => async (dispatch) => {
+	try {
+		const { data } = await api.fetchPosts();
+
+		dispatch({ type: 'FETCH_ALL', payload: data });
+	} catch (error) {
+		console.log(error.message);
+	}
+
+
+	const action = { type: 'FETCH_ALL', payload: [] }
+	
+}
